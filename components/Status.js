3d1b67ff62cfa0b8fc2e2f9ea421931c55eab9ff
callmedane <qcddsemrero@tip.edu.ph>
2025-10-20 18:52:13 +0800

    Updated Status.js with NetInfo and message bubble for connectivity

--- components/Status.js
+++ components/Status.js
@@ -1,16 +1,32 @@
-// components/Status.js
 import React from "react";
 import { View, Text, StyleSheet, StatusBar, Platform } from "react-native";
 import Constants from "expo-constants";
+import NetInfo from "@react-native-community/netinfo";
 
 export default class Status extends React.Component {
   state = {
-    info: "wifi", 
+    isConnected: true,
   };
 
+  componentDidMount() {
+    this.unsubscribe = NetInfo.addEventListener((state) => {
+      const connected =
+        state.isConnected && state.isInternetReachable !== false;
+      this.setState({ isConnected: connected });
+    });
+    NetInfo.fetch().then((state) => {
+      const connected =
+        state.isConnected && state.isInternetReachable !== false;
+      this.setState({ isConnected: connected });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe && this.unsubscribe();
+  }
+
   render() {
-    const { info } = this.state;
-    const isConnected = info !== "none";
+    const { isConnected } = this.state;
     const backgroundColor = isConnected ? "white" : "red";
 
     const statusBar = (
@@ -22,15 +38,21 @@ export default class Status extends React.Component {
     );
 
     const messageContainer = (
-      <View style={styles.messageContainer} pointerEvents={"none"}>
+      <View style={styles.messageContainer} pointerEvents="none">
         {statusBar}
-        {!isConnected && (
-          <View style={styles.bubble}>
-            <Text style={styles.text}>No network connection</Text>
-          </View>
-        )}
+        <View
+          style={[
+            styles.bubble,
+            { backgroundColor: isConnected ? "green" : "red" },
+          ]}
+        >
+          <Text style={styles.text}>
+            {isConnected ? "Connected to Internet" : "No network connection"}
+          </Text>
+        </View>
       </View>
     );
+
     if (Platform.OS === "ios") {
       return (
         <View style={[styles.status, { backgroundColor }]}>
@@ -63,9 +85,9 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderRadius: 20,
-    backgroundColor: "red",
   },
   text: {
     color: "white",
+    fontWeight: "bold",
   },
 });
